9be366e55572b8a10174f3e8d9d1755933bbcd23
paul-ventura-git <j.paul.ventura@gmail.com>
2024-11-28 18:05:42 -0500

    App clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useSelector } from 'react-redux'
-import './App.css'
 
 function App() {
   const { counter } = useSelector( state => state.counter)
   return (
     <>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={ () => {}}>
           count is { counter }
