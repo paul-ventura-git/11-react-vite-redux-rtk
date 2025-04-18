ff2c73a0e5735b890f54ef3b7b065b2e96c111f6
paul-ventura-git <j.paul.ventura@gmail.com>
2025-04-18 01:47:18 -0500

    Axios

--- src/slices/pokemon/thunks.js
+++ src/slices/pokemon/thunks.js
@@ -1,11 +1,16 @@
+import { pokemonApi } from "../../api/pokemonApi";
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice";
 
 export const getPokemons = ( page ) => {
   return async ( dispatch ) => {
     dispatch( startLoadingPokemons() );
-
+/*
     const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${ page * 10}`);
     const data = await resp.json();
+    console.log(data);
+*/
+    const resp = await pokemonApi.get(`pokemon?limit=10&offset=${ page * 10}`);
+    const data = resp.data;
     console.log(data);
 
     dispatch( setPokemons({ pokemons:data.results, page:page+1}));
